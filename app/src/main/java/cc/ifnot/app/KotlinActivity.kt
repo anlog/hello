c3b42827d22ef6430034ce89115db2a84fb91f3e
hello <hello@ifnot.cc>
2020-05-20 18:03:36 +0800

    add rxjava support

--- app/src/main/java/cc/ifnot/app/KotlinActivity.kt
+++ app/src/main/java/cc/ifnot/app/KotlinActivity.kt
@@ -4,6 +4,10 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.core.SingleEmitter
+import io.reactivex.rxjava3.core.SingleOnSubscribe
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 /**
  * author: dp
@@ -14,9 +18,19 @@ import androidx.appcompat.app.AppCompatActivity
 @SuppressLint("Registered")
 open class KotlinActivity : AppCompatActivity() {
 
+    companion object {
+        private val TAG = "KotlinActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+        Log.d(TAG, "thread: " + Thread.currentThread(), Exception())
+        Single.create(SingleOnSubscribe<Int> { Log.d(TAG, "thread: " + Thread.currentThread(), Exception()) }).subscribeOn(Schedulers.single())
+                .ignoreElement().blockingAwait()
+
+        return
         // test
         val list = ArrayList<String>();
         list.add("a=b");
@@ -31,7 +45,7 @@ open class KotlinActivity : AppCompatActivity() {
 }
 
 
-private fun List<String>.testRet() = map{it}
+private fun List<String>.testRet() = map { it }
 
 
 private fun List<String>.toMapInternal(): Map<String, String> {
@@ -39,7 +53,7 @@ private fun List<String>.toMapInternal(): Map<String, String> {
     val map = LinkedHashMap<String, String>()
     for (s in this) {
         val ss = s.split("=", limit = 2)
-        if(ss.size == 2) {
+        if (ss.size == 2) {
             map[ss[0]] = ss[1]
         }
     }
