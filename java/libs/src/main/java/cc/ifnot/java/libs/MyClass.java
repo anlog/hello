d46648c3d39dfcdad9c8cd7956bca266b1945370
hello <hello@ifnot.cc>
2020-06-19 00:34:12 +0800

    run run run
    Signed-off-by: hello <hello@ifnot.cc>

--- java/libs/src/main/java/cc/ifnot/java/libs/MyClass.java
+++ java/libs/src/main/java/cc/ifnot/java/libs/MyClass.java
@@ -1,4 +1,44 @@
 package cc.ifnot.java.libs;
 
+import java.util.concurrent.Callable;
+
+import cc.ifnot.libs.utils.Lg;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.CompletableObserver;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class MyClass {
+
+    static {
+        Lg.tag("MyClass");
+        Lg.showMore(true);
+    }
+
+    public static void main(String[] args) {
+        Completable.fromCallable(new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                Lg.d("call");
+                return null;
+            }
+        }).subscribeOn(Schedulers.computation()).subscribe(new CompletableObserver() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Lg.d("onSubscribe");
+            }
+
+            @Override
+            public void onComplete() {
+                Lg.d("onComplete");
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Lg.d("onError");
+            }
+        });
+    }
 }
