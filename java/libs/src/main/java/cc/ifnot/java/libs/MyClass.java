c0bf66fa3b43e45ea2902174a8fb4ff50e18e09e
hello <hello@ifnot.cc>
2020-06-19 09:32:42 +0800

    clean up
    Signed-off-by: hello <hello@ifnot.cc>

--- java/libs/src/main/java/cc/ifnot/java/libs/MyClass.java
+++ java/libs/src/main/java/cc/ifnot/java/libs/MyClass.java
@@ -7,7 +7,6 @@ import cc.ifnot.libs.utils.Lg;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.CompletableObserver;
-import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
@@ -20,12 +19,9 @@ public class MyClass {
 
     public static void main(String[] args) throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
-        Completable.fromCallable(new Callable<Void>() {
-            @Override
-            public Void call() throws Exception {
-                Lg.d("call");
-                return null;
-            }
+        Completable.fromCallable((Callable<Void>) () -> {
+            Lg.d("call");
+            return null;
         }).subscribeOn(Schedulers.computation()).subscribe(new CompletableObserver() {
             @Override
             public void onSubscribe(@NonNull Disposable d) {
