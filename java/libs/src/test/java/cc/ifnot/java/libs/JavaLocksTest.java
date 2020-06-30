e2a580cbffbb098511bf9cc2b4e2f578978d4577
hello <hello@ifnot.cc>
2020-06-30 14:38:50 +0800

    p c
    Signed-off-by: hello <hello@ifnot.cc>

--- java/libs/src/test/java/cc/ifnot/java/libs/JavaLocksTest.java
+++ java/libs/src/test/java/cc/ifnot/java/libs/JavaLocksTest.java
@@ -29,6 +29,90 @@ class JavaLocksTest {
     }
 
     @Test
+    void testSynchronizedWaitNotify() {
+        Lg.d("testSynchronizedWaitNotify");
+
+        int threads = 10;
+
+        class Counter {
+            private int count = 0;
+
+            int getCount() {
+                return count;
+            }
+
+            int getAndIncrease() {
+                final int c = count;
+                count = c + 1;
+                return c;
+            }
+
+            int getAndDecrease() {
+                final int c = count;
+                count = c - 1;
+                return c;
+            }
+
+        }
+
+        final Object lock = new Object();
+        final Counter counter = new Counter();
+
+        for (int i = 0; i < threads / 2; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        synchronized (lock) {
+                            Lg.d("count p: %s", counter.getCount());
+                            while (counter.getCount() == 5) {
+                                Lg.d("wait p %d", counter.getCount());
+                                try {
+                                    lock.wait();
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            Lg.d("count p: %d -> %d", counter.getAndIncrease(),
+                                    counter.getCount());
+                            lock.notifyAll();
+                        }
+                    }
+                }
+            }).start();
+        }
+
+        for (int i = 0; i < threads / 2; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        synchronized (lock) {
+                            Lg.d("count c: %s", counter.getCount());
+                            while (counter.getCount() == 0) {
+                                Lg.d("wait c %d", counter.getCount());
+                                try {
+                                    lock.wait();
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            Lg.d("count c: %d -> %d", counter.getAndDecrease(),
+                                    counter.getCount());
+                            lock.notifyAll();
+                        }
+                    }
+                }
+            }).start();
+
+        }
+
+        while (true) {
+            Thread.yield();
+        }
+    }
+
+    @Test
     void testAtomicInteger() {
         Lg.d("test testAtomicInteger");
 
