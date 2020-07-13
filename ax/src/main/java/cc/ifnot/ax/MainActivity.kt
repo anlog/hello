9b9a89b54185380b035ae3bdaa2a0d7eac3da6db
hello <hello@ifnot.cc>
2020-07-13 11:06:43 +0800

    androidx init
    Signed-off-by: hello <hello@ifnot.cc>

---
+++ ax/src/main/java/cc/ifnot/ax/MainActivity.kt
@@ -0,0 +1,12 @@
+package cc.ifnot.ax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
