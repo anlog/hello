e2c571f0c3ebbd462df1022f700e1918d33db554
hello <hello@ifnot.cc>
2020-07-22 23:16:19 +0800

    quicksort
    Signed-off-by: hello <hello@ifnot.cc>

--- java/libs/src/test/java/cc/ifnot/java/libs/alg/Search.java
+++ java/libs/src/test/java/cc/ifnot/java/libs/alg/Search.java
@@ -13,9 +13,79 @@ import cc.ifnot.libs.utils.Lg;
  */
 class Search {
 
+
+    @Test
+    void bubbleSort() {
+        int[] arr = new int[]{1, -1, 3, 5, 1, 34, 5, 1};
+
+        Lg.d(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    arr[i] = arr[i] + arr[j];
+                    arr[j] = arr[i] - arr[j];
+                    arr[i] = arr[i] - arr[j];
+                }
+            }
+        }
+        Lg.d(Arrays.toString(arr));
+
+    }
+
+    @Test
+    void quickSortTest() {
+        int[] arr = new int[]{1, -1, 3, 5, 1, 34, 5, 1};
+
+        Lg.d(Arrays.toString(arr));
+
+        quickSort(arr, 0, arr.length - 1);
+        Lg.d(Arrays.toString(arr));
+    }
+
+    private void quickSort(int[] arr, int start, int end) {
+        int i = start;
+        int j = end;
+        if (i > j) return;
+        int k = arr[i];
+
+
+        while (i < j) {
+            while (i < j && arr[j] > k) {
+                j--;
+            }
+            if (i < j) {
+                arr[i] = arr[j];
+                i++;
+            }
+            while (i < j && arr[i] < k) {
+                i++;
+            }
+            if (i < j) {
+                arr[j] = arr[i];
+                j--;
+            }
+        }
+        arr[i] = k;
+        quickSort(arr, start, i - 1);
+        quickSort(arr, i + 1, end);
+    }
+
+
+    @Test
+    void reverseNum() {
+        int num = 123456789;
+        do {
+            System.out.print(num % 10);
+        } while ((num = num / 10) > 0);
+
+        Lg.d();
+        Lg.d("%s", 10 + 1 >> 1);
+    }
+
+
     @Test
     void binarySearchTest() {
-        final int search = binarySearch(new int[]{-1, 0, 1, 2, 3, 4, 99, 1000}, 99);
+        final int search = binarySearch(new int[]{-1, 0, 1, 2, 3, 3, 4, 99, 99, 1000}, 3);
         Lg.d("ret: %s", search);
 //        Arrays.binarySearch()
     }
@@ -34,6 +104,9 @@ class Search {
             } else if (k > arr[mid]) {
                 lo = mid + 1;
             } else {
+                while (mid > 0 && arr[mid - 1] == arr[mid]) {
+                    mid--;
+                }
                 return mid;
             }
         }
