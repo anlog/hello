8c17bcc634e0d4d4e3723ac0600c585d95988cfa
hello <hello@ifnot.cc>
2021-04-22 16:36:15 +0800

    cleanup
    Signed-off-by: hello <hello@ifnot.cc>

--- java/libs/src/main/java/cc/ifnot/java/libs/socket/Client.java
+++
@@ -1,9 +0,0 @@
-package cc.ifnot.java.libs.socket;
-
-/**
- * author: dp
- * created on: 2020/7/17 3:15 PM
- * description:
- */
-class Client {
-}
--- java/libs/src/test/java/cc/ifnot/java/libs/kt_test.kt
+++
@@ -1,5 +0,0 @@
-package cc.ifnot.java.libs
-
-fun main(){
-    println("hello world")
-}
